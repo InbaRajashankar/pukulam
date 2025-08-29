443a79d383c030882f80a99057f77f8615d01449
Inba <rkrishinba@gmail.com>
2025-08-29 15:51:55 -0500

    moved threejs to own division

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,23 @@
+const container = document.getElementById('three-container');
+
 // set up the scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x88ccee); // Sky blue background
+scene.background = new THREE.Color(0x00000);
 
 // set up the camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 20, 20);
+const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+camera.position.set(0, 10, 20);
 
 // set up the renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize(container.clientWidth, container.clientHeight);
+container.appendChild(renderer.domElement);
 
 // add lights
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-directionalLight.position.set(10, 20, 10);
+directionalLight.position.set(16, 20, 10);
 scene.add(directionalLight);
 
 // load texture
@@ -27,7 +29,7 @@ const texture = textureLoader.load('/textures/texture.jpg', () => {
 });
 
 // add sphere
-const geometry = new THREE.SphereGeometry( 5, 32, 32 ); 
+const geometry = new THREE.SphereGeometry( 10, 64, 64 ); 
 const material = new THREE.MeshBasicMaterial( { map: texture } ); 
 const sphere = new THREE.Mesh( geometry, material );
 scene.add(sphere)
@@ -43,14 +45,17 @@ controls.dampingFactor = 0.05;
 
 // handle window resize
 window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.aspect = container.clientWidth / container.clientHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(container.clientWidth, container.clientHeight);
 });
 
 // animation loop
 function animate() {
   requestAnimationFrame(animate);
+  sphere.rotation.y += 0.005;
+
+
   controls.update();
   renderer.render(scene, camera);
 }
